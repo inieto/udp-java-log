37837ff5f315300ff7e21e338974af6c50d18ec5
Ignacio Nieto <nietofarias@gmail.com>
2017-04-18 17:24:05 -0300

    log

--- src/main/java/com/inieto/UDPServer.java
+++ src/main/java/com/inieto/UDPServer.java
@@ -3,9 +3,11 @@ import java.net.*;
 
 class UDPServer {
   public static void main(String args[]) throws Exception {
+    System.out.println("Starting UDP Server");
     DatagramSocket serverSocket = new DatagramSocket(9876);
-    byte[] receiveData = new byte[1024];
-    byte[] sendData = new byte[1024];
+    byte[] receiveData = new byte[16];
+    byte[] sendData = new byte[16];
+    System.out.println("Listening on 9876");
     while(true) {
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
       serverSocket.receive(receivePacket);
