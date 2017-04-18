233f1e09a7cd039916885a62bb700714b41c1a7d
Ignacio Nieto <nietofarias@gmail.com>
2017-04-18 16:57:55 -0300

    initial

---
+++ UDPServer.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+
+class UDPServer {
+  public static void main(String args[]) throws Exception {
+    DatagramSocket serverSocket = new DatagramSocket(9876);
+    byte[] receiveData = new byte[1024];
+    byte[] sendData = new byte[1024];
+    while(true) {
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      serverSocket.receive(receivePacket);
+      String sentence = new String( receivePacket.getData());
+      System.out.println("RECEIVED ("+receivePacket.getAddress()+":"+receivePacket.getPort()+"): " + sentence);
+    }
+  }
+}
