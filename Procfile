d4676a8c2debfddc6a4df0b86c97d62418f43702
Ignacio Nieto <nietofarias@gmail.com>
2017-04-18 17:36:19 -0300

    test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:	play run -javaagent:/app/localLibs/newrelic.jar -Dnewrelic.config.file=/app/conf/newrelic.yml --http.port=$PORT $PLAY_OPTS
--- src/main/java/com/inieto/UDPServer.java
+++ src/main/java/com/inieto/UDPServer.java
@@ -4,10 +4,11 @@ import java.net.*;
 class UDPServer {
   public static void main(String args[]) throws Exception {
     System.out.println("Starting UDP Server");
-    DatagramSocket serverSocket = new DatagramSocket(80);
+    int port = Integer.valueOf(System.getenv("PORT"));
+    DatagramSocket serverSocket = new DatagramSocket(port);
     byte[] receiveData = new byte[16];
     byte[] sendData = new byte[16];
-    System.out.println("Listening on 80");
+    System.out.println("Listening on "+port);
     while(true) {
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
       serverSocket.receive(receivePacket);
